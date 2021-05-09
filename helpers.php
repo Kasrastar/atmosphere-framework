410a9a483002e600c6699ede650b0b597daf2799
pialechini <mobinzohourian@gmail.com>
2021-05-09 10:18:40 +0430

    Develop helper functions

--- helpers.php
+++ helpers.php
@@ -1,91 +1,116 @@
 <?php
 
 
-if (! function_exists('telegram'))
+if ( ! function_exists('telegram'))
 {
 	/**
 	 * Class used to send messages to specific chat
 	 *
 	 * @return \BotFramework\Facilities\TelegramRequest
 	 */
-	function telegram() : \BotFramework\Facilities\TelegramRequest
+	function telegram () : \BotFramework\Facilities\TelegramRequest
 	{
 		return new \BotFramework\Facilities\TelegramRequest;
 	}
 }
 
 
-if (! function_exists('response'))
+if ( ! function_exists('response'))
 {
 	/**
 	 * Smart class to easily send messages to the current chat
 	 *
 	 * @return \BotFramework\Facilities\Response
 	 */
-	function response() : \BotFramework\Facilities\Response
+	function response () : \BotFramework\Facilities\Response
 	{
 		return new \BotFramework\Facilities\Response;
 	}
 }
 
 
-if (! function_exists('update'))
+if ( ! function_exists('update'))
 {
 	/**
 	 * Get current update instance
 	 *
 	 * @return \Longman\TelegramBot\Entities\Update
 	 */
-	function update() : \Longman\TelegramBot\Entities\Update
+	function update () : \Longman\TelegramBot\Entities\Update
 	{
 		return \BotFramework\Facilities\Supports\CurrentUpdate::get();
 	}
 }
 
-if (! function_exists('message'))
+if ( ! function_exists('message'))
 {
 	/**
 	 * Get current message instance
 	 *
 	 * @return \Longman\TelegramBot\Entities\Message
 	 */
-	function message() : \Longman\TelegramBot\Entities\Message
+	function message () : \Longman\TelegramBot\Entities\Message
 	{
 		return update()->getMessage();
 	}
 }
 
-if (! function_exists('chat'))
+if ( ! function_exists('chat'))
 {
 	/**
 	 * Get current chat instance
 	 *
 	 * @return \Longman\TelegramBot\Entities\Chat
 	 */
-	function chat() : \Longman\TelegramBot\Entities\Chat
+	function chat () : \Longman\TelegramBot\Entities\Chat
 	{
 		return update()->getMessage()->getChat();
 	}
 }
 
 
-if (! function_exists('user'))
+if ( ! function_exists('user'))
 {
 	/**
 	 * Get current user instance
 	 *
 	 * @return \Longman\TelegramBot\Entities\User
 	 */
-	function user() : \Longman\TelegramBot\Entities\User
+	function user () : \Longman\TelegramBot\Entities\User
 	{
 		return update()->getMessage()->getFrom();
 	}
 }
 
-if (! function_exists('str_contains'))
+if ( ! function_exists('str_contains'))
 {
-	function str_contains($string, $search) : bool
+	/**
+	 * Checks $string if containing $search or not
+	 *
+	 * @param $string
+	 * @param $search
+	 *
+	 * @return bool
+	 */
+	function str_contains ($string, $search) : bool
 	{
 		return strpos($string, $search) !== false;
 	}
 }
+
+if ( ! function_exists('db'))
+{
+	/**
+	 * Query Builder
+	 *
+	 * @param string $table
+	 *
+	 * @return \Illuminate\Database\Query\Builder
+	 */
+	function db_table ($table) : \Illuminate\Database\Query\Builder
+	{
+		return \Illuminate\Database\Capsule\Manager::table($table);
+	}
+}
+
+
